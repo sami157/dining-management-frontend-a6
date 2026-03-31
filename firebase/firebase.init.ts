0626a2ecfce3781e36872cc3fd0eb1d22af0aaca
sami157 <sami.eee157@gmail.com>
2026-04-01 03:02:49 +0600

    firebase secrets setup

---
+++ firebase/firebase.init.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+function getEnv(name: string): string {
+  const value = process.env[name];
+
+  if (!value) {
+    throw new Error(`Missing environment variable: ${name}`);
+  }
+
+  return value;
+}
+
+const firebaseConfig = {
+  apiKey: getEnv("NEXT_PUBLIC_FIREBASE_API_KEY"),
+  authDomain: getEnv("NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"),
+  projectId: getEnv("NEXT_PUBLIC_FIREBASE_PROJECT_ID"),
+  storageBucket: getEnv("NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET"),
+  messagingSenderId: getEnv("NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"),
+  appId: getEnv("NEXT_PUBLIC_FIREBASE_APP_ID"),
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
